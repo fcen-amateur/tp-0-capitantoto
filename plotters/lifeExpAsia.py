7fdfdaaaa79415808539ce1f2baf0483d40f8166
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2024-04-16 18:26:27 -0300

    plotter con retorno roto en asia

---
+++ plotters/lifeExpAsia.py
@@ -0,0 +1,26 @@
+import seaborn.objects as so
+from gapminder import gapminder
+
+
+def plot():
+    figura = (
+        so.Plot(
+            gapminder[gapminder.continent == "Asia"],
+            x="year",
+            y="lifeExp",
+            color="country",
+        )
+        .add(so.Line())
+        .label(
+            title="Expectativa de vida en Oceanía",
+            x="Año",
+            y="Expectativa de vida",
+            color="País",
+        )
+    )
+    return dict(
+        descripcion="Expectativa de vida en países de Oceanía a lo largo del tiempo",
+        autor="La cátedra",
+        mygod=42,
+        figura=figura,
+    )
